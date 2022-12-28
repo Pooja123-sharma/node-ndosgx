09ddfae65203465153e562feab9461ba9dd9c77e
Pooja123-sharma <117913332+Pooja123-sharma@users.noreply.github.com>
2022-12-28 14:44:37 +0530

    1 file modified

--- index.js
+++ index.js
@@ -5,3 +5,12 @@ console.log(x.fill(10,30));
 // pop
 let a = [3,5,7,9];
 console.log(a.pop());
+
+// push
+let b = [65,75,85,90];
+c = [];
+console.log(b.push([]));
+
+// filter
+let y = [40,50,60,70,80];
+console.log(y.filter(y => y.length > y));
